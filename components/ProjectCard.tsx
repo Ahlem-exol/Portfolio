a329c58031e9f45c801a3183133d345314d8c50c
chelliahlem98gmail.com <chelliahlem98@gmail.com>
2022-06-18 13:54:08 +0100

    Update

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -102,6 +102,8 @@ const ProjectCard: FunctionComponent<{
               </a>
               <a
                 href={deployed_url}
+                rel="noreferrer"  
+                target={'_blank'}
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
               >
                 <AiFillProject /> <span>Project</span>
